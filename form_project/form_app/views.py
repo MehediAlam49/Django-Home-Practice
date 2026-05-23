cdd158663e0b20105893d0325c65798330387928
MehediAlam49 <mehedialam806@gmail.com>
2026-05-23 09:48:30 +0600

    update code for show output data from database

--- form_project/form_app/views.py
+++ form_project/form_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from form_app.models import *
 
 # Create your views here.
+def product_list(request):
+    product_data=productModel.objects.all()
+    
+    context={
+        'product_data':product_data
+    }
+    return render(request, 'product_list.html',context)
